9e225d966510b4e16b99f2b146a25c2c3741e781
Jerome Faria <jerome.faria@gmail.com>
2015-08-22 09:09:55 +0100

    fixed typos

--- functions.sh
+++ functions.sh
@@ -45,62 +45,74 @@ function upgradenode {
 }
 
 # Create a new directory and enter it
+
 function take() {
   mkdir $1
   cd $1
 }
 
 # Checks for MusicBrainz tag on MusicBrainz Picard
+
 function brainz(){
   open -a "Musicbrainz Picard" $1
 }
 
 # Copies and opens in iTunes
+
 function itunes(){
   open -a "iTunes" $1
 }
 
 # Convert audio files with Max
+
 function max(){
   open /Applications/Max.app $1
 }
 
 # Downloads YouTube videos
+
 function ydl(){
   youtube-dl -citw $1
 }
 
 # Open with
+
 function oa(){
   open -a $1 $2
 }
 
 # Grab all href links from a webpage
+
 function grab() {
   curl $1 | grep -o -E 'href="([^"#]+)"' | cut -d'"' -f2
 }
 
 # Start Yeoman server
+
 function gs() {
   grunt serve &
 }
 
 # Start Yeoman server
+
 function gsd() {
   grunt serve:dist &
 }
 
 # Kill Yeoman server for dist
+
 function ks() {
   jobs -l | awk '{print $3}' | xargs kill
 }
 
 # Convert flac to mp3
+
 function flac2mp3() {
   for a in *.flac; do < /dev/null ffmpeg -i "$a" -qscale:a 0 "${a[@]/%flac/mp3}"; done
 }
 
 # Read specific line from file
+
 function rl() {
   head -$2 $1 | tail -1
 }
