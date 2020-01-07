cacdfd46ccb9242771050e50d56b96517851e8d9
Jerome Faria <jerome.faria@gmail.com>
2020-01-07 17:06:53 +0000

    Add digga()

--- functions.sh
+++ functions.sh
@@ -80,6 +80,11 @@ function phpserver() {
   php -S "${ip}:${port}";
 }
 
+# Run `dig` and display the most useful info
+function digga() {
+  dig +nocmd "$1" any +multiline +noall +answer;
+}
+
 }
 
 # Downloads YouTube videos
