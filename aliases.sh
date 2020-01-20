d8d1cd8fb83f9b9d6badee4649b5fdc33de0016e
Jerome Faria <jerome.faria@gmail.com>
2020-01-20 09:49:07 +0000

    fix(zsh): disable fasd key bindings

--- aliases.sh
+++ aliases.sh
@@ -70,9 +70,9 @@ alias sd='fasd -sid'     # interactive directory selection
 alias sf='fasd -sif'     # interactive file selection
 alias z='fasd_cd -d'     # cd, same functionality as j in autojump
 alias zz='fasd_cd -d -i' # cd with interactive selection
-alias v='f -e vim'       # quick opening files with vim
-alias m='f -e mplayer'   # quick opening files with mplayer
-alias o='a -e xdg-open'  # quick opening files with xdg-open
+# alias v='f -e vim'       # quick opening files with vim
+# alias m='f -e mplayer'   # quick opening files with mplayer
+# alias o='a -e xdg-open'  # quick opening files with xdg-open
 
 # Folders
 alias h="cd $HOME"
